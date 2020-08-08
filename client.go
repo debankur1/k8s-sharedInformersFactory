d64e2f1a2a097480da57bde9b1618aeddd6a1205
dchatterjee1 <debankur_chatterjee@intuit.com>
2020-08-08 21:27:07 +0530

    Shared Informers

--- client.go
+++ client.go
@@ -1,1 +1,69 @@
-package k8s_sharedInformers
+package main
+
+import (
+	"flag"
+	"fmt"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
+	"path/filepath"
+)
+//Make sure Kubeconfig is in path
+func main(){
+	var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+	client, err := kubernetes.NewForConfig(config)
+	//Listening to Pods events
+	stop := make(chan struct{})
+	defer close(stop)
+	defer runtime.HandleCrash()
+	factory := informers.NewSharedInformerFactory(client,0)
+	informer := factory.Core().V1().Pods().Informer()
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			fmt.Println(key)
+			if err!=nil{
+				panic(err)
+			}
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(newObj)
+			fmt.Println(key)
+			if err!=nil{
+				panic(err)
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			fmt.Println(key)
+			if err!=nil{
+				panic(err)
+			}
+		},
+	})
+	informer.Run(stop)
+
+	if !cache.WaitForCacheSync(stop, informer.HasSynced) {
+		return
+	}
+	<-stop
+}
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
+}
